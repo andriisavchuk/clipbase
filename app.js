5f6b0e45abcaf8c73a3925f849a1ac70b90a0ab0
Andrii Savchuk <andriksr@gmail.com>
2017-11-05 19:54:56 +0200

    set up basic server connection

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 const express = require('express');
+
+
+// Set up axpress application const
+const app = express();
+
+// Set up port
+const port = process.env.port || 3000;
+
+// Listening of the requests
+app.listen(port, () => {
+  console.log(`Connected to the server on port ${port}`);
+});
