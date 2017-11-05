df3ef17ab1867b0644e4a1f6723786cb4a2d77c1
Andrii Savchuk <andriksr@gmail.com>
2017-11-05 20:41:33 +0200

    added handlebar middleware

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 
 
-// Set up axpress application const
+// Set up express application const
 const app = express();
+
+// Set up Handlebars Middleware
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
+// Set up Index Route
+app.get('/', (req, res) => {
+  const title = 'Welcome to my NodeJS App';
+  res.render('index', {
+    title
+  });
+});
+
+// Set up About Route
+app.get('/about', (req, res) => {
+  res.render('about');
+});
 
 // Set up port
 const port = process.env.port || 3000;
