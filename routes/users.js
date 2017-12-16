ad28ba152fb5aa565ccc813ca7b270a354442644
Andrii Savchuk <andriksr@gmail.com>
2017-12-16 09:34:58 +0200

    aadded validation for the form

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,5 @@
+import { error } from 'util';
+
 const express = require('express');
 const mongoose = require('mongoose');
 const router = express.Router();
@@ -16,8 +18,24 @@ router.get('/registration', (req, res) => {
 router.post('/registration', (req, res) => {
   let errors = [];
 
-  if (req.body.password) {
+  if (req.body.password != req.body.password2) {
+      errors.push({text: 'Passwords do not match'});
+  }
 
+  if (req.body.password.length < 4) {
+    errors.push({text: 'Password must be at least 4 characters'});
+  }
+
+  if (errors.length > 0) {
+    res.render('users/registration', {
+      errors: errors,
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+      password2: req.body.password2
+    });
+  } else {
+    res.send('passed');
   }
 });
 
