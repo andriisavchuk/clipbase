c4c603b16190857b386cd1929dc605d954aa86bf
Andrii Savchuk <andriksr@gmail.com>
2017-12-21 22:03:07 +0200

    added functionality for double email checking

--- routes/users.js
+++ routes/users.js
@@ -39,26 +39,34 @@ router.post('/registration', (req, res) => {
       password2: req.body.password2
     });
   } else {
-    const newUser = new User({
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password
-    });
-
-    bcrypt.genSalt(10, (err, salt) => {
-      bcrypt.hash(newUser.password, salt, (err, hash) => {
-        if (err) throw err;
-        newUser.password = hash;
-        newUser.save()
-        .then(user => {
-          req.flash('success_msg', 'You are now registered. Please log in.');
-          res.redirect('/users/login');
-        })
-        .catch(err => {
-          console.log(err);
-          return;
+    User.findOne({email: req.body.email})
+    .then(user => {
+      if (user) {
+        req.flash('error_msg', 'User with such email is already registered.');
+        res.redirect('/users/registration');
+      } else {
+        const newUser = new User({
+          name: req.body.name,
+          email: req.body.email,
+          password: req.body.password
         });
-      });
+
+        bcrypt.genSalt(10, (err, salt) => {
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) throw err;
+            newUser.password = hash;
+            newUser.save()
+            .then(user => {
+              req.flash('success_msg', 'You are now registered. Please log in.');
+              res.redirect('/users/login');
+            })
+            .catch(err => {
+              console.log(err);
+              return;
+            });
+          });
+        });
+      }
     });
   }
 });
