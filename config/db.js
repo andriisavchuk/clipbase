bcfb4295b209143d8a28eb87e16721ba73468c6b
Andrii Savchuk <andriksr@gmail.com>
2018-06-15 21:57:07 -0500

    remove config folder

--- config/db.js
+++
@@ -1,5 +0,0 @@
-if (process.env.NODE_ENV === 'production') {
-  module.exports = {mongoURI: 'mongodb://andy:andy@ds145273.mlab.com:45273/clipbase-prod'}
-} else {
-  module.exports = {mongoURI: 'mongodb://localhost/clipbase'}
-}
